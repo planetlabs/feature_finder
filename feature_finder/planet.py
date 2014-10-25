37c34ea23a553068e5157b4a89c0d70a4474ed15
Jennifer Reiber Kyle <reiberkyle@gmail.com>
2014-10-25 15:20:40 -0600

    Break out testing logic for planet.py

--- feature_finder/planet.py
+++ feature_finder/planet.py
@@ -3,30 +3,6 @@ import geojson
 import json
 
 
-def run():
-    test()
-
-def test():
-    dallas_tx = {"coordinates": [-96.7967,32.7695], "type": "Point"}
-    cedar_rapids_ia_airport = {"coordinates": [-91.7046, 41.8854], "type": "Point"}
-    points = [dallas_tx, cedar_rapids_ia_airport]
-
-    params = {
-        "intersects": geojson.dumps(points[0]),
-    } 
-    data = planet_query(params)
-    scenes_0 = data.json()["features"]
-
-    scenes_1 = get_intersecting_scenes(points[0])
-    print '{} ?= {}'.format(len(scenes_0), len(scenes_1))
-
-    scenes_2 = get_intersecting_scenes(points[1])
-
-    scenes_tot = get_scenes_by_points(points)
-    print '{} + {} ?= {}'.format(len(scenes_1), len(scenes_2), len(scenes_tot))
-
-
-
 def get_key(key_file='../.key'):
     with open('../.key', 'r') as f:
         key = f.read()
@@ -75,5 +51,3 @@ def save(json_dict, filename):
     with open(filename, 'w') as f:
         f.write(json.dumps(json_dict))    
 
-if __name__ == '__main__':
-    run()
---
+++ tests/test_planet.py
@@ -0,0 +1,39 @@
+import unittest
+
+import geojson
+
+from feature_finder import planet
+
+
+
+class Test(unittest.TestCase):
+    def setup(self):
+        self.dallas_tx = {"coordinates": [-96.7967,32.7695], "type": "Point"}
+        self.cedar_rapids_ia_airport = {"coordinates": [-91.7046, 41.8854], "type": "Point"}
+        self.test_points = [self.dallas_tx, self.cedar_rapids_ai_airport]
+
+    def test_planet_query(self):
+        test_point = self.dallas_tx
+        params = {
+            "intersects": geojson.dumps(test_point),
+        } 
+        data = planet.planet_query(params)
+        scenes_0 = data.json()["features"]
+
+        self.assertTrue(len(scenes_0) >= 2)
+
+    def test_get_intersecting_scenes(self):
+
+        scenes_1 = planet.get_intersecting_scenes(self.test_points[0])
+        self.assertTrue(len(scenes_1) >= 2)
+
+    def test_get_scenes_by_points(self):
+
+        scenes_1 = planet.get_intersecting_scenes(self.test_points[0])
+        scenes_2 = planet.get_intersecting_scenes(self.test_points[1])
+
+        scenes_tot = planet.get_scenes_by_points(self.test_points)
+        self.assertEqual(len(scenes_tot), len(scenes_1) + len(scenes_2))
+
+if __name__ == '__main__':
+    unittest.main()
