82149994cb3e23085b3960429958d2f589b18534
Jennifer Reiber Kyle <reiberkyle@gmail.com>
2014-10-25 15:12:19 -0600

    init.sh: update packaging, add repository directory to python path

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+BASE_DIR=`dirname $0`
+
+# change to the directory this script is running in, so work will
+# be relative to a known path
+TOP=$(cd $(dirname $0) && pwd)
+
 sudo apt-get install -y --force-yes \
     build-essential python-dev \
-    python-pip
+    python-pip python-nose python-requests
 
-pip install requests
+echo $TOP | sudo tee /usr/local/lib/python2.7/dist-packages/feature_finder.pth
